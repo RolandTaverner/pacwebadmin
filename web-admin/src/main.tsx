1957b76fbdd46b9d7a793a1601d7747b7828e906
Alexander Verbitsky <roland.taverner@gmail.com>
2025-11-09 12:49:15 +0300

    add redux store

--- web-admin/src/main.tsx
+++ web-admin/src/main.tsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById('root')
+
+if (container) {
+  const root = createRoot(container)
+
+  root.render(
+    <Provider store={store}>
+      <StrictMode>
+        <App />
+      </StrictMode>,
+    </Provider>,
+  )
+} else {
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  )
+}
