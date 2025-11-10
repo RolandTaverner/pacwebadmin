e27e488fe3786ee77e23380ad1b3ba71f53d24f8
Alexander Verbitsky <roland.taverner@gmail.com>
2025-11-10 23:26:23 +0300

    setup layout

--- web-admin/src/components/app/App.tsx
+++ web-admin/src/components/app/App.tsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+import Categories from '../categories/Categories';
+
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -28,6 +24,7 @@ function TabPanel(props: TabPanelProps) {
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
       {...other}
+      style={{ width: '100%' }}
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
@@ -54,29 +51,33 @@ function App() {
 
   return (
     <>
-      <Container sx={{ flexGrow: 1, bgcolor: 'primary.light', display: 'flex', height: 30, width: 1000, justifySelf: 'flex-start', verticalAlign: "center" }} >
+      <Container sx={{ bgcolor: 'primary.light', display: 'flex', height: 30, width: 1000 }} >
         Header
       </Container>
-      <Box
-        sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 340, width: 1000, justifySelf: 'flex-start' }}
-      >
-        <Tabs
-          orientation="vertical"
-          //variant="scrollable"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical tabs example"
-          sx={{ borderRight: 1, borderColor: 'divider', alignContent: 'flex-start' }}
+      <Box sx={{display: 'grid', gridTemplateColumns: '150px auto' }}>
+
+        <Box
+          sx={{ flexGrow: 1, bgcolor: 'background.paper' }}
         >
-          <Tab label="Dashboard" {...a11yProps(0)} />
-          <Tab label="PAC" {...a11yProps(1)} />
-          <Tab label="Proxy rules" {...a11yProps(2)} />
-          <Tab label="Hosts" {...a11yProps(3)} />
-          <Tab label="Proxies" {...a11yProps(4)} />
-          <Tab label="Categories" {...a11yProps(5)} />
-          <Tab label="About" {...a11yProps(6)} />
-        </Tabs>
-        <Box sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', justifyContent: 'flex-start' }}>
+          <Tabs
+            orientation="vertical"
+            variant="standard"
+            value={value}
+            onChange={handleChange}
+            aria-label="Vertical tabs"
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+          >
+            <Tab label="Dashboard" {...a11yProps(0)} />
+            <Tab label="PAC" {...a11yProps(1)} />
+            <Tab label="Proxy rules" {...a11yProps(2)} />
+            <Tab label="Hosts" {...a11yProps(3)} />
+            <Tab label="Proxies" {...a11yProps(4)} />
+            <Tab label="Categories" {...a11yProps(5)} />
+            <Tab label="About" {...a11yProps(6)} />
+          </Tabs>
+        </Box>
+
+        <Box sx={{ bgcolor: 'background.paper'}}>
           <TabPanel value={value} index={0}>
             Item One
           </TabPanel>
@@ -93,12 +94,13 @@ function App() {
             Item Five
           </TabPanel>
           <TabPanel value={value} index={5}>
-            Item Six
+            <Categories />
           </TabPanel>
           <TabPanel value={value} index={6}>
             Item Seven
           </TabPanel>
         </Box>
+
       </Box>
     </>
   )
---
+++ web-admin/src/components/categories/Categories.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './Categories.css'
+
+import Box from '@mui/material/Box';
+
+function Categories() {
+  const [value, setValue] = useState(0);
+
+  return (
+    <>
+        <Box sx={{ bgcolor: 'primary.dark' }}>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+            <div>Test</div>
+        </Box>
+    </>
+  )
+}
+
+export default Categories
